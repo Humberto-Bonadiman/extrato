6055f401aebfa4645089e54f3fcca70b432787e2
Humberto-Bonadiman <humberto_bonadiman@hotmail.com>
2022-12-25 10:59:40 -0400

    style: estiliza página gerar-extrato

--- src/pages/GerarExtrato.tsx
+++ src/pages/GerarExtrato.tsx
@@ -13,12 +13,19 @@ function GerarExtrato() {
   const [valor, setValor] = useState(0);
   const [tipo, setTipo] = useState('');
 
+  const alterDateFormat = (date: String) => {
+    const day = date.substring(0, 2);
+    const month = date.substring(3, 5);
+    const year = date.substring(6, 10);
+    return `${year}-${month}-${day}`;
+  }
+
   const generateTransaction = async (event: { preventDefault: () => void; }) => {
     event.preventDefault();
     const dateTime = new Date().toLocaleString();
     const id = localStorage.getItem('id_conta') || 0;
     const bodyFunction: transferenciaInterface = {
-      dataTransferencia: dateTime,
+      dataTransferencia: alterDateFormat(dateTime),
       valor,
       tipo,
       nomeOperadorTransacao: operador,
@@ -26,10 +33,16 @@ function GerarExtrato() {
     };
     const result = await fetchTransferencia(bodyFunction);
     const POST = 201;
-    // const body = await result.json();
     if (result.status === POST) {
       navigate('/extrato');
     }
+  }
+
+  const enableOperator = () => {
+    if (tipo === "ENTRADA" || tipo === "SAIDA") {
+      return false
+    }
+    return true;
   }
 
   const submit = (event: { preventDefault: () => void; }) => {
@@ -67,7 +80,7 @@ function GerarExtrato() {
   return(
     <Container className="transaction-form">
       <Form className="form">
-      <Form.Group className="mb-3">
+        <Form.Group className="mb-3">
           <Form.Label
             data-testid="transaction__label-value"
           >
@@ -83,34 +96,40 @@ function GerarExtrato() {
             onChange={ ({ target }) => setValor(Number(target.value)) }
           />
         </Form.Group>
-        <Form.Check
-          type="radio"
-          label="depósito"
-          id="radio__deposito"
-          value="DEPOSITO"
-          onChange={ ({ target }) => setTipo(target.value) }
-        />
-        <Form.Check
-          type="radio"
-          label="saque"
-          id="radio__saque"
-          value="SAQUE"
-          onChange={ ({ target }) => setTipo(target.value) }
-        />
-        <Form.Check
-          type="radio"
-          label="transferência entrada"
-          id="radio__entrada"
-          value="ENTRADA"
-          onChange={ ({ target }) => setTipo(target.value) }
-        />
-        <Form.Check
-          type="radio"
-          label="transferência saída"
-          id="radio__saida"
-          value="SAIDA"
-          onChange={ ({ target }) => setTipo(target.value) }
-        />
+        <Form.Group className="mb-3">
+          <Form.Check
+            type="radio"
+            label="&nbsp;&nbsp;&nbsp;&nbsp;Depósito"
+            name="tipo"
+            id="radio__deposito"
+            value="DEPOSITO"
+            onChange={ ({ target }) => setTipo(target.value) }
+          />
+          <Form.Check
+            type="radio"
+            label="&nbsp;&nbsp;&nbsp;&nbsp;Saque"
+            name="tipo"
+            id="radio__saque"
+            value="SAQUE"
+            onChange={ ({ target }) => setTipo(target.value) }
+          />
+          <Form.Check
+            type="radio"
+            label="&nbsp;&nbsp;&nbsp;&nbsp;Transferência entrada"
+            name="tipo"
+            id="radio__entrada"
+            value="ENTRADA"
+            onChange={ ({ target }) => setTipo(target.value) }
+          />
+          <Form.Check
+            type="radio"
+            label="&nbsp;&nbsp;&nbsp;&nbsp;Transferência saída"
+            name="tipo"
+            id="radio__saida"
+            value="SAIDA"
+            onChange={ ({ target }) => setTipo(target.value) }
+          />
+        </Form.Group>
         <Form.Group className="mb-3">
           <Form.Label
             data-testid="transaction__label-nome"
@@ -119,12 +138,13 @@ function GerarExtrato() {
           </Form.Label>
           <Form.Control
             type="text"
+            disabled={ enableOperator() }
             placeholder="João da Silva Souza"
             data-testid="transaction__nome"
             onChange={ ({ target }) => setOperador(target.value) }
           />
         </Form.Group>
-        
+          
         <Button
           data-testid="transaction__button-tranfer"
           id="transaction__button-tranfer"
